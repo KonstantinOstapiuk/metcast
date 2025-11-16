bad8a30538973c66de794d6ab931a2cf598aa41c
KonstantinOstapiuk <kostyadstapuk@gmail.com>
2025-11-16 14:54:45 +0600

    htpps

--- esets/scripts/script.js
+++ esets/scripts/script.js
@@ -4,7 +4,7 @@ const weather = document.getElementById('weather')
 
 function getWetherByCity(city){
     //&aqi=no - не отправлять доп информацию
-    fetch('http://api.weatherapi.com/v1/current.json?key=075045bbad254b9fb5764120251611&q=' + city + '&aqi=no')
+    fetch('https://api.weatherapi.com/v1/current.json?key=075045bbad254b9fb5764120251611&q=' + city + '&aqi=no')
     
     .then(function(response) {
         return response.json()
@@ -21,7 +21,6 @@ function getWetherByCity(city){
         console.log(error)
     })
 }
-
 searchBtn.addEventListener('click', function(){
     let city = cityInput.value
     if (city === ''){
